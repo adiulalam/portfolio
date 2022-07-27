ba306fe2218310f3508416d7e414709f95b52fde
Adiul Alam Adil <adiulalam@gmail.com>
2022-07-27 18:26:32 +0100

    loading changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import Nav from "./components/Nav";
 import Home from "./components/Pages";
 import Admin from "./Authentication/admin";
 import { SkeletonTheme } from "react-loading-skeleton";
-import PacmanLoader from "react-spinners/PacmanLoader";
-import CircleLoader from "react-spinners/CircleLoader";
-import BarLoader from "react-spinners/BarLoader";
-import MoonLoader from "react-spinners/MoonLoader";
+import Loading from "./connection/loading";
 import ContentObjects from "./connection/connection";
 
 function App() {
-  const [randNum, setRandNum] = useState(null);
-  useEffect(() => {
-    setRandNum(Math.floor(Math.random() * 4));
-  }, []);
-
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -29,30 +21,7 @@ function App() {
     fetchData();
   }, []);
 
-  if (isLoading)
-    return (
-      (document.body.style.background = "#000000"),
-      (
-        <div
-          style={{
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          {randNum === 0 ? (
-            <PacmanLoader speedMultiplier="2" color="#ffffff" size="80px" />
-          ) : randNum === 1 ? (
-            <CircleLoader color="#ffffff" size="200px" />
-          ) : randNum === 2 ? (
-            <BarLoader color="#ffffff" height="10px" width="300px" />
-          ) : (
-            <MoonLoader color="#ffffff" size="80px" />
-          )}
-        </div>
-      )
-    );
+  if (isLoading) return <Loading />;
 
   const portfolioProps = data;
 
@@ -65,17 +34,21 @@ function App() {
       >
         <Nav portfolioProps={portfolioProps}>
           <Switch>
-          <Route exact path="/admin" render={(props) => <Admin {...props} />} />
-          <Route
-            exact
-            path="/"
-            render={(props) => (
-              <Home portfolioProps={portfolioProps} {...props} />
-            )}
-          />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
-      </Nav>
+            <Route
+              exact
+              path="/admin"
+              render={(props) => <Admin {...props} />}
+            />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home portfolioProps={portfolioProps} {...props} />
+              )}
+            />
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        </Nav>
       </SkeletonTheme>
     </div>
   );
