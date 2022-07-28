5ee2fbbb0c7d7e3e031ded059eefef1335dd7ca2
Adiul Alam Adil <adiulalam@gmail.com>
2022-07-29 00:34:04 +0100

    db

--- src/Authentication/profile.js
+++ src/Authentication/profile.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton from "./login";
+// import LoginButton from "./login";
 import LogoutButton from "./logout";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const {
+    getAccessTokenSilently,
+    loginWithRedirect,
+    user,
+    isAuthenticated,
+    isLoading,
+  } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+  const [userToken, setUserToken] = useState("");
 
-  if (!isAuthenticated) {
-    return <LoginButton />;
-  }
+  useEffect(() => {
+    (async function login() {
+      if (!isLoading && !user && !isAuthenticated) {
+        await loginWithRedirect();
+      } else {
+        try {
+          const token = await getAccessTokenSilently({
+            audience: "hasura",
+          });
+          setUserToken(token);
+          // console.log("token---->", token);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    })();
+  }, [isLoading, getAccessTokenSilently]);
+
+  console.log("token---->", user);
 
   return (
     isAuthenticated && (
-      <div>
+      <div style={{ "background-color": "white" }}>
         <LogoutButton />
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        {/* <p>{token}</p> */}
       </div>
     )
   );
