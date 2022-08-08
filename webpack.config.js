713d1433dc0de41b51898e72c5cce9c6ed699f7a
Adiul Alam Adil <adiulalam@gmail.com>
2022-08-08 23:29:29 +0100

    docker

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-import createWebpackConfigAsync from "@expo/webpack-config/webpack";
-
-new createWebpackConfigAsync.EnvironmentPlugin({
-    NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-    DEBUG: false,
-  });
