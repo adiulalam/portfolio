d71d974e320857f474c01b57870c40c75e7ab171
Adiul Alam Adil <adiulalam@gmail.com>
2022-08-26 23:38:11 +0100

    cleanup

--- src/Authentication/components/form.jsx
+++ src/Authentication/components/form.jsx
@@ -24,19 +24,6 @@ const Form = ({
           </label>
         </div>
         <div class="flex col-span-3">
-          {/* <input
-            class={` bg-gray-200 appearance-none border-2 ${
-              value ? "" : "border-red-500"
-            } rounded w-full py-2 px-4 text-gray-700 leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
-            type="text"
-            name={name}
-            value={value}
-            id={textValue[id]}
-            readOnly={name === id ? true : false}
-            onChange={(e) => {
-              onTextChange(e, index);
-            }}
-          /> */}
           <Input
             name={name}
             value={value}
