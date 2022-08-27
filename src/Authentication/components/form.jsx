eaca3acb91033d8c3263574aeece9dab8ecb7b4c
Adiul Alam Adil <adiulalam@gmail.com>
2022-08-27 21:10:31 +0100

    datepicker

--- src/Authentication/components/form.jsx
+++ src/Authentication/components/form.jsx
@@ -1,5 +1,5 @@
 import { ButtonAdd, ButtonDelete, ButtonReset } from "./button";
-import { Input } from "./form_components";
+import { DatePick, Input, TextArea } from "./form_components";
 
 const Form = ({
   name,
@@ -24,16 +24,40 @@ const Form = ({
           </label>
         </div>
         <div class="flex col-span-3">
-          <Input
-            name={name}
-            value={value}
-            id={id}
-            textValue={textValue}
-            readOnly={name === id ? true : false}
-            onTextChange={(e) => {
-              onTextChange(e, index);
-            }}
-          />
+          {name === "description" ? (
+            <TextArea
+              name={name}
+              value={value}
+              id={id}
+              textValue={textValue}
+              readOnly={name === id ? true : false}
+              onTextChange={(e) => {
+                onTextChange(e, index);
+              }}
+            />
+          ) : name === "projectdate" ? (
+            <DatePick
+              name={name}
+              value={value}
+              id={id}
+              textValue={textValue}
+              readOnly={name === id ? true : false}
+              onTextChange={(e) => {
+                onTextChange(e, index);
+              }}
+            />
+          ) : (
+            <Input
+              name={name}
+              value={value}
+              id={id}
+              textValue={textValue}
+              readOnly={name === id ? true : false}
+              onTextChange={(e) => {
+                onTextChange(e, index);
+              }}
+            />
+          )}
         </div>
         <div class="flex justify-center md:sm:justify-start">
           <ButtonReset name={name} handleReset={handleReset} index={index} />
--- src/Authentication/components/form_components.jsx
+++ src/Authentication/components/form_components.jsx
@@ -1,11 +1,9 @@
-const Input = ({
-  name,
-  value,
-  textValue,
-  id,
-  onTextChange,
-  index,
-}) => {
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const Input = ({ name, value, textValue, id, onTextChange, index }) => {
   return (
     <input
       class={` bg-gray-200 appearance-none border-2 ${
@@ -23,4 +21,55 @@ const Input = ({
   );
 };
 
-export { Input };
+const TextArea = ({ name, value, textValue, id, onTextChange, index }) => {
+  return (
+    <textarea
+      class={` resize-y file:bg-gray-200 appearance-none border-2 ${
+        value ? "" : "border-red-500"
+      } rounded w-full py-2 px-4 text-gray-700 leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
+      type="text"
+      name={name}
+      value={value}
+      id={textValue[id]}
+      readOnly={name === id ? true : false}
+      onChange={(e) => {
+        onTextChange(e, index);
+      }}
+    ></textarea>
+  );
+};
+
+const DatePick = ({ name, value, textValue, id, onTextChange, index }) => {
+  const [startDate, setStartDate] = useState(new Date(value));
+  return (
+    <>
+      <DatePicker
+      class={` resize-y file:bg-gray-200 appearance-none border-2 ${
+        value ? "" : "border-red-500"
+      } rounded w-full py-2 px-4 text-gray-700 leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
+     
+      dateFormat="yyyy-MM-dd"
+        // selected={new Date(value)}
+        name={name}
+        value={startDate}
+        id={textValue[id]}
+        // readOnly={name === id ? true : false}
+        // onChange={(e, date) => {
+        //   onTextChange(e, index);
+        // }}
+        yearDropdownItemNumber={10}
+        scrollableYearDropdown={true}
+        showYearDropdown
+        showMonthDropdown
+        
+        selected={startDate}
+        onChange={(date) => {
+          setStartDate(date);
+        onTextChange()
+        }}
+      />
+    </>
+  );
+};
+
+export { Input, TextArea, DatePick };
