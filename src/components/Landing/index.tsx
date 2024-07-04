8795e8831ee01e6c14d6508c9999f7cdefab65ed
Adiul Alam Adil <44062730+adiulalam@users.noreply.github.com>
2024-07-04 01:28:30 +0100

    Delete src/components/Landing directory

--- src/components/Landing/index.tsx
+++
@@ -1,6 +0,0 @@
-import { LandingAboutMe } from "./LandingAboutMe";
-import { LandingDescription } from "./LandingDescription";
-import { LandingHome } from "./LandingHome";
-import { LandingInfo } from "./LandingInfo";
-
-export { LandingAboutMe, LandingHome, LandingInfo, LandingDescription };
